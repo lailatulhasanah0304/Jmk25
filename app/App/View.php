9f6fb1cd5f03446b5c234777480b30af37c24b30
evanalifian <evanalifian@gmal.com>
2025-11-13 14:05:48 +0700

    membuat default value untuk parameter

--- app/App/View.php
+++ app/App/View.php
@@ -4,7 +4,7 @@
 namespace Jmk25\App;
 
 class View {
-  public static function render(string $view, $model): void {
+  public static function render(string $view, $model = []): void {
     require_once __DIR__ . "/../Views/template/header.php";
     require_once __DIR__ . "/../Views/pages/" . $view . ".php";
     require_once __DIR__ . "/../Views/template/footer.php";
