137297e843520f843174ef7ea746e07efcd78882
evanalifian <evanalifian@gmal.com>
2025-11-23 16:33:07 +0700

    add new database configuration for production

--- config/database.php
+++ config/database.php
@@ -7,6 +7,11 @@ function getConfigDB(): array {
         "path" => "mysql:host=localhost:3306;dbname=jmk25",
         "username" => "root",
         "password" => ""
+      ],
+      "prod" => [
+        "path" => "mysql:host=localhost:3306;dbname=jmk25",
+        "username" => "root",
+        "password" => ""
       ]
     ]
   ];
